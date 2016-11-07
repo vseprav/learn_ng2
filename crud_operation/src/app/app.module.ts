fb2eb089c3625795c608cfe0da535c7ffaa28877
Andrii Peliak <vseprav@gmail.com>
2016-11-07 11:34:32 +0200

    add pages

--- crud_operation/src/app/app.module.ts
+++ crud_operation/src/app/app.module.ts
@@ -5,16 +5,29 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HomeComponent,
+    UsersComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'users', component: UsersComponent },
+      { path: 'posts', component: PostsComponent },
+      {path: '**', redirectTo: '/'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
