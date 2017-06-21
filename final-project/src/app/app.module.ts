8cf30f69a0ec9660c8fffb8756e6cdcacceb801b
Andrii Peliak <vseprav@gmail.com>
2017-06-21 15:45:40 +0300

    add pages

--- final-project/src/app/app.module.ts
+++ final-project/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FpNavigationComponent } from './fp-navigation/fp-navigation.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users',      component: UsersComponent },
+  { path: 'posts',  component: PostsComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    FpNavigationComponent
+    FpNavigationComponent,
+    HomeComponent,
+    UsersComponent,
+    PostsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
