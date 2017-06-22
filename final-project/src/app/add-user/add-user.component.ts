6ffd0de7f8bccba3bca7e2c73e7cafa86dab3e01
Andrii Peliak <vseprav@gmail.com>
2017-06-22 14:24:53 +0300

    add form validation

--- final-project/src/app/add-user/add-user.component.ts
+++ final-project/src/app/add-user/add-user.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { User }    from '../models/user';
 
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
   styleUrls: ['./add-user.component.css']
 })
+
 export class AddUserComponent implements OnInit {
+
+  model = new User(12, '', '');
 
   constructor() { }
 
--- final-project/src/app/app.module.ts
+++ final-project/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FpNavigationComponent } from './fp-navigation/fp-navigation.component';
@@ -33,7 +34,8 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
